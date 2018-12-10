f135b355dd846b16e8fc53e836bf9c61e7d03449
Vlad <vlad.sivh@gmail.com>
2018-12-11 01:34:16 +0300

    111

--- client/src/main.js
+++ client/src/main.js
@@ -1,1 +1,49 @@
-console.log('main.js');
+import $ from "jquery";
+
+console.log('main');
+
+
+$(document).ready(function () {
+
+    var header = $('header');
+    var pageWidth = $(window).width();
+  
+    $('body').on('click', '.scroller', function(){
+      $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top - 100
+      }, 800, 'easeOutQuart');
+    });
+  
+    //functions to run on scroll
+    $(window).scroll(function(){
+      //only run parallax if on desktop screen
+      //(mobile parallax interferes with content consumption)
+      if ( pageWidth > 800 ) {
+        headerAnimate();
+        parallax();
+      }
+    });
+  
+    function parallax(){
+      var scrolled = $(window).scrollTop();
+      $('.has-parallax').css(
+        'top', (scrolled*0.3)+'px'
+      );
+    }
+  
+    function headerAnimate(){
+      var scrollTop = $(window).scrollTop();
+      if(scrollTop > 20) {
+        header.addClass('scrolled');
+      } else {
+        header.removeClass('scrolled');
+      }
+    }
+  
+    $(".js-soon").on("click", function (e) {
+      e.preventDefault();
+      $(this).text("Coming Soon...");
+      resetText($(this), "Learn more");
+    });
+    
+  });
